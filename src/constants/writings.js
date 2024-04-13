0d21d5da79fb5bf97b70ceebd247a03db1ea8b8e
akashakku <72681497+akashakku@users.noreply.github.com>
2024-04-13 11:30:21 +0530

    Update writings.js
    Did some cleaning up

--- src/constants/writings.js
+++ src/constants/writings.js
@@ -1,30 +1,12 @@
 // Constant Strings
-export const CV_LINK =
-  "https://drive.google.com/drive/folders/1mw6Dzlk6cI5mlO5hdeY6yhqk7AMnFkBA?usp=sharing";
-export const GMAIL = "ashooshukla@gmail.com";
-export const LINKEDIN_LINK =
-  "https://www.linkedin.com/in/ashutoshshukla9871946911/";
-export const LINKEDIN_TEXT = "@ashutoshshukla9871946911";
-
-export const GOOGLESCHOLAR_LINK = "https://scholar.google.com/citations?hl=en&user=GZTgMxoAAAAJ";
-export const GOOGLESCHOLAR_TEXT = "@ashutoshshukla_gscholar";
-export const RESEARCHGATE_LINK = "https://www.researchgate.net/profile/Ashutosh-Shukla-29";
-export const RESEARCHGATE_TEXT = "@ashutoshshukla_rgate";
-
-export const YOUTUBE_LINK = "https://www.youtube.com/@noclickbaitphysics5938";
-export const YOUTUBE_TEXT = "@noclickbaitphysics5938";
-
-export const CONTACT_TEXT =
-  `You can find me here on internet.
-  Feel free to contact me.`;
-
-
-
-
-
-
-export const TEACHING_TEXT =
-  "I am one of the teachers at iiser and i teach in this subject in that department i am king of the world and top of the earth.";
+export const BLOGS = [
+  {
+    title: "What is Life?",
+    description: "link to a video of stupid falling kids",
+    dateofentry: "/today",
+    references: "1. here is another link. 2. here is another another link",
+  },
+];
 
 export const TEACHINGS = [
   {
@@ -34,99 +16,10 @@ export const TEACHINGS = [
     interval: "The blog page is still under construction. Please visit other pages.",
   },
   {
-    title: "Teaching Assistantship",
+    title: "What is Lorem Ipsum?",
     description:
       "Teaching assistant in PH4233 Advanced Physics Lab with Prof G V, Pavan Kumar",
     interval: "Spring semester 2023",
   },
-  {
-    title: "PMRF Teaching",
-    description:
-      "Teaching the course PHY202 at Interdisciplinary school of science, Pune University",
-    interval: "Spring semester 2024",
-    extraText: "Course Notes can be found",
-    extraLink:
-      "https://drive.google.com/drive/folders/1UEd5wxSPtN3BxXrp_leZ0KF2YsSGJvT7",
-  },
-  {
-    title: "PMRF Teaching",
-    description:
-      "Taught Physics 102 course at Interdisciplinary School of Science at Savitribai Phule Pune University",
-    interval: "Autumn semester 2023",
-    extraText: "Course Notes can be found",
-    extraLink:
-      "https://drive.google.com/drive/folders/1Tr9Pzi7CY01wf2tW8PBymkKCJjXGbN06?usp=sharing",
-  },
-  {
-    title: "PMRF Teaching",
-    description:
-      "Taught Physics 202 course at Interdisciplinary School of Science at Savitribai Phule Pune University",
-    interval: "Spring semester 2023",
-    extraText: "Course Notes can be found",
-    extraLink:
-      "https://drive.google.com/drive/folders/1UEd5wxSPtN3BxXrp_leZ0KF2YsSGJvT7",
-  },
 ];
 
-// Publication Data
-export const PUBLICATIONS = [
-  {
-    title:
-      "Strain-Enabled Defect Migration and Defect Activation in Monolayer",
-    subTitle: "2D Mater., 10 (4), 045009 (2023)",
-    link: "https://dx.doi.org/10.1088/2053-1583/aceb74",
-    authors: [
-      "Divya Nechiyil",
-      "Gokul M. A.",
-      "Ashutosh Shukla",
-      "G V Pavan Kumar",
-      "Atikur Rahman",
-    ],
-    description:
-      "Collaborative work where i contributed in the spectroscopy.",
-  },
-  {
-    title:
-      "Optothermal Trapping of Fluorescent Nanodiamonds using a Drop-casted Gold Nanoparticle",
-    subTitle: "Opt. Lett., OL 48, 2937 (2023)",
-    link: "https://opg.optica.org/ol/abstract.cfm?uri=ol-48-11-2937",
-    authors: [
-      "Ashutosh Shukla",
-      "Sunny Tiwari",
-      "Ayan Majumder",
-      "Kasturi Saha",
-      "G V Pavan Kumar",
-    ],
-    description:
-      "We show that fluorescent nanodiamonds can be trapped using a gold nanoparticle as a heat source. The nanodiamond is trapped on the heat source gold particle by an opto-thermoelectric field generated due to the presence of a surfactant.",
-  },
-  {
-    title:
-      "Directing monolayer tungsten disulfide photoluminescence using a bent-plasmonic nanowire on a mirror cavity",
-    subTitle: "Eur. Phys. J. Spec. Top. 231, 807–813 (2022)",
-    link: "https://doi.org/10.1140/epjs/s11734-022-00511-y",
-    authors: [ "Shailendra Chaubey", "Sunny Tiwari", "Ashutosh Shukla", ],
-    description:
-      
-      "We show that a bent silver nanowire placed on a gold mirror acts as a waveguide and transports the photoluminescence emitted at one end from a 2D material can be transported along the length and emitted at the kink in a small range of angles.",
-      
-  },
-];
-
-export const HOME_TEXT =
-  `I am a Prime Minister’s Research Fellow (PMRF) at IISER Pune.
-I like studying and teaching science.`;
-
-
-export const ABOUT_TEXT =
-  `Ashutosh, a scientist so bright,
-uses lasers to trap particles in sight.
-Communicating science with passion and zeal,
-breaking down barriers, making knowledge real.`
-
-export const PROFESSIONAL_BIO =
-  `I am Ashutosh Shukla, currently pursuing my research as a scholar at the Indian Institute of Science Education and Research (IISER) Pune, India.
-I utilize optical tweezers to investigate colloidal and nanoparticle systems, with a focus on developing novel techniques to trap and manipulate nanoparticles for applications in materials science and biomedicine.
-
-In addition to my research, I am an experienced science communicator and outreach enthusiast. I run two YouTube channels, 'No Clickbait Physics' and 'Vlog My PhD,' where I break down complex scientific concepts and make them accessible to non-academic audiences.
-I have a keen interest in interacting with school and college students, where I explain science and physics concepts using simple demonstrations. I am committed to making science accessible and engaging for everyone and to inspiring the next generation of scientists.`
