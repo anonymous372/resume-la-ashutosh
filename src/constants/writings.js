d3af16aad08a3f9fccdf041b3757a9ec5e403776
akashakku <72681497+akashakku@users.noreply.github.com>
2024-04-13 11:04:46 +0530

    Update writings.js
    Testing the blog writing ability using some lorem ipsum text

--- src/constants/writings.js
+++ src/constants/writings.js
@@ -28,9 +28,9 @@ export const TEACHING_TEXT =
 
 export const TEACHINGS = [
   {
-    title: "Teaching Assistantship",
+    title: "What is Lorem Ipsum?",
     description:
-      "Teaching assistant in PH3134 Optics at IISER Pune with Prof. G V Pavan Kumar",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     interval: "Autumn Semester 2022",
   },
   {
